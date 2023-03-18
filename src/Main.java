c6c074a6fae8381855511c6009b1586fab829e3d
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 19:02:32 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
+    static int[][] copyPart(int[][] arr, int c1, int c2, int r1, int r2) {
+        int width = 1 + (c2 - c1);
+        int height = 1 + (r2 - r1);
+
+        int[][] res = new int[height][width];
+
+        for (int i=0; i < res.length; i++) {
+            for (int j=0; j < res[i].length; j++) {
+                res[i][j] = arr[r1+i][c1+j];
+            }
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         // Skriv, och testa, en metod som returnerar en utvald del av en 2D-array
         //
@@ -7,6 +22,20 @@ public class Main {
         //   3 4 5 6 7    kolumn  1 - 3           4 5 6
         //   4 5 6 7 8    rad     1 - 2
 
+        int[][] arr = {
+                {1, 2, 3, 4, 5},
+                {2, 3, 4, 5, 6},
+                {3, 4, 5, 6, 7},
+                {4, 5, 6, 7, 8}
+        };
 
+        int[][] arr2 = copyPart(arr,1,3,1,2);
+
+        for(int i=0; i < arr2.length; i++) {
+            for (int j=0; j < arr2[i].length; j++) {
+                System.out.print(arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
