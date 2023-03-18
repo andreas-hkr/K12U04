326e92f5155623165d6529dd9de6484b577741a6
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 18:46:11 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skriv, och testa, en metod som returnerar en utvald del av en 2D-array
+        //
+        //   1 2 3 4 5
+        //   2 3 4 5 6  - - - - - - - - - - - >   3 4 5
+        //   3 4 5 6 7    kolumn  1 - 3           4 5 6
+        //   4 5 6 7 8    rad     1 - 2
+
+
+    }
+}
